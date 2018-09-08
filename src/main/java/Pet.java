80c4d5f7229231de838bf600575d174724fc0598
Don <dmiesle@mac.com>
2018-09-08 16:41:12 -0400

    entertainment and rest increment added

--- src/main/java/Pet.java
+++ src/main/java/Pet.java
@@ -3,24 +3,62 @@ public class Pet {
 
 	private String petName;
 	private int foodLevel;
+	private int entertained;
+	private int rested;
 
 	// this is my constructor
-	public Pet(String petName, int foodLevel) {
+	public Pet(String petName, int foodLevel, int entertained, int rested) {
 		this.petName = petName;
 		this.foodLevel = foodLevel;
+		this.entertained = entertained;
+		this.rested = rested;
 	}
 
 	// this is the getter
 	public String getPetName() {
 		return petName;
 	}
+
+	// food changes
 	public int getFoodLevel() {
 		return foodLevel;
 	}
+
 	public void feed() {
 		foodLevel = foodLevel + 1;
 	}
+
 	public void consume() {
 		foodLevel = foodLevel - 1;
 	}
+	// entertainment changes
+
+	public int getEntertained() {
+		return entertained;
+	}
+
+	public void attend() {
+		entertained = entertained + 1;
+
+	}
+
+	public void neglect() {
+		entertained = entertained - 1;
+
+	}
+
+	public int getRested() {
+		return rested;
+	}
+
+	public void sleep() {
+		rested = rested +1;
+		
+	}
+
+	public void active() {
+		rested = rested -1;
+		
+	}
+
 }
--- src/test/java/VirtualPetTest.java
+++ src/test/java/VirtualPetTest.java
@@ -4,28 +4,67 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class VirtualPetTest {
-	Pet underTest = new Pet("My Pet", 1);
-	
+	Pet underTest = new Pet("My Pet", 1, 1, 1);
+
 	@Test
 	public void doesThePetHaveANameMyPet() {
 		String check = underTest.getPetName();
-		assertThat (check, is(new String("My Pet")));
+		assertThat(check, is(new String("My Pet")));
 	}
+
 	@Test
-	public void isTheFoodLevel1 () {
+	public void isTheFoodLevel1() {
 		int check = underTest.getFoodLevel();
-		assertThat (check, is(1));
+		assertThat(check, is(1));
 	}
+
 	@Test
-	public void isThePetWellFed2IfItStartsAt1AndIsFed () {
+	public void isThePetWellFed2IfItStartsAt1AndIsFed() {
 		underTest.feed();
 		int check = underTest.getFoodLevel();
-		assertThat (check, is(2));
+		assertThat(check, is(2));
 	}
+
 	@Test
-	public void isThePetWellFed0IfItStartsAt1AndConsumes () {
+	public void isThePetWellFed0IfItStartsAt1AndConsumes() {
 		underTest.consume();
 		int check = underTest.getFoodLevel();
-		assertThat (check, is(0));
+		assertThat(check, is(0));
+	}
+
+	@Test
+	public void isThePetEntertainedAt1() {
+		int check = underTest.getEntertained();
+		assertThat(check, is(1));
+	}
+	@Test
+	public void doesThePetsEntertainementGoUpTo2IfAttend() {
+		underTest.attend();
+		int check = underTest.getEntertained();
+		assertThat(check, is(2));
+	}
+	@Test
+	public void doesThePetsEntertainementGoDownTo0IfNeglect() {
+		underTest.neglect();
+		int check = underTest.getEntertained();
+		assertThat(check, is(0));
+	}
+	
+	@Test
+	public void isThePetRestedAt1() {
+		int check = underTest.getRested();
+		assertThat(check, is(1));
+	}
+	@Test
+	public void doesThePetsRestedGoUpTo2IfSleep() {
+		underTest.sleep();
+		int check = underTest.getRested();
+		assertThat(check, is(2));
+	}
+	@Test
+	public void doesThePetsRestedGoDownTo0IfActive() {
+		underTest.active();
+		int check = underTest.getRested();
+		assertThat(check, is(0));
 	}
 }
